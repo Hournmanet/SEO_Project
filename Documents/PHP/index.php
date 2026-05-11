290b732ac23dc1772cc670216ab999ba5997028a
Your Name <your-email@example.com>
2026-05-11 14:46:50 +0700

    scannow 50% project

---
+++ Documents/PHP/index.php
@@ -0,0 +1,18 @@
+ <?php 
+ 
+//  $a = "Hello World";
+//  echo ($a);
+
+
+//$a ="Hello World";
+//echo (strlen($a));  // check how many characters in the string
+
+//  $a = "Hello World";
+//  echo($a . " " . "How are you?");  // concatenate two strings (. = +)
+
+//  $a = "Hello World.  sdkmsdmf";
+//  echo str_word_count($a);  // check how many words in the string
+
+// $a = "Hello World";
+// echo (str_replace("World", "Everyone", $a));  // replace a word in the string.   (replace from world to everyone in the string a)
+?>
---
+++ Documents/PHP/number.php
@@ -0,0 +1,10 @@
+<?php
+
+// $a = 12;
+// echo ($a);  // print the value of a.   (echo = print)
+
+
+$a = "12.32";
+echo (is_numeric($a));  // check if the value of a is numeric or not
+
+?>
